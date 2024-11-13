977da5444670927e5bf373ff66ea60b23a33bee9
Lorenzo Martinez <lmar190504@gmail.com>
2024-11-13 14:30:10 -0300

    Borrado de comentarios

--- src/components/ProfessorDashboard.jsx
+++ src/components/ProfessorDashboard.jsx
@@ -1,28 +1,28 @@
 import { useState } from 'react';
-import { fetchStudentsAndSubjects, fetchSubjectsByProfessorId } from '../services/api'; // Asegúrate de que la nueva función esté importada
+import { fetchStudentsAndSubjects, fetchSubjectsByProfessorId } from '../services/api'; 
 import "./ProfessorDashboard.css";
 
 const ProfessorDashboard = () => {
-    const [students, setStudents] = useState([]); // Estado para los estudiantes
-    const [subjects, setSubjects] = useState([]); // Estado para las materias
-    const [professorId, setProfessorId] = useState(''); // Estado para el ID del profesor
-    const [error, setError] = useState(null); // Estado para manejar errores
+    const [students, setStudents] = useState([]); 
+    const [subjects, setSubjects] = useState([]); 
+    const [professorId, setProfessorId] = useState(''); 
+    const [error, setError] = useState(null); 
 
     const handleFetchStudents = async (e) => {
-        e.preventDefault(); // Evita el comportamiento por defecto del formulario
-        setError(null); // Resetea el error antes de hacer la solicitud
+        e.preventDefault(); 
+        setError(null); 
         try {
-            const studentsData = await fetchStudentsAndSubjects(professorId); // Llama a la función con el ID del profesor
-            console.log(studentsData); // Verifica la estructura de los datos
-            setStudents(studentsData || []); // Asegúrate de que se establezca un arreglo vacío si no hay datos
+            const studentsData = await fetchStudentsAndSubjects(professorId); 
+            console.log(studentsData); 
+            setStudents(studentsData || []); 
 
-            // Obtén las materias del profesor
+            
             const subjectsData = await fetchSubjectsByProfessorId(professorId);
-            console.log(subjectsData); // Verifica la estructura de los datos
-            setSubjects(subjectsData || []); // Asegúrate de que se establezca un arreglo vacío si no hay datos
+            console.log(subjectsData); 
+            setSubjects(subjectsData || []); 
         } catch (error) {
             console.error('Error fetching students or subjects:', error);
-            setError('Error al obtener los alumnos inscriptos o las materias.'); // Manejo de errores
+            setError('Error al obtener los alumnos inscriptos o las materias.'); 
         }
     };
 
